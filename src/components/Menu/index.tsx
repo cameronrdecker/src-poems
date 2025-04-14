b1020768c8045594f4ee052d3eb00cb65d90b05e
Cameron Decker <cameronrdecker@users.noreply.github.com>
2025-04-13 20:43:15 -0400

    initial changes

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Menu.module.css";
+
+export default function Menu() {
+    return (
+      <nav className={styles.menu} box-="square contain:!top" >
+        <strong is-="badge" >/src</strong>
+  
+      </nav>
+    );
+  }
+  
