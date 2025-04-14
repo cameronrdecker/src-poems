b9152e2a946763adaa0e465fdff92ef45e4892b4
Cameron Decker <cameronrdecker@users.noreply.github.com>
2025-04-14 11:44:45 -0400

    Define component

--- src/app/(poems)/main/page.tsx
+++ src/app/(poems)/main/page.tsx
@@ -1,12 +1,12 @@
 import FunctionDefinition from "@/components/Functions/FunctionDefinition"
 import FunctionCall from "@/components/Functions/FunctionCall"
-import Command from "@/components/Command"
+import Define from "@/components/Define"
 import Variable from "@/components/Variable"
 
 export default function Main(){
     return(
         <FunctionDefinition title="Main">
-            <Command>let</Command> <Variable>result</Variable> = <FunctionCall title="live a life" link="/live-a-life" />;
+            <Define/> <Variable>result</Variable> = <FunctionCall title="live a life" link="/live-a-life" />;
         </FunctionDefinition>
     )
 }
